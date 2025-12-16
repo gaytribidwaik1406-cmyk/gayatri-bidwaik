c9ff49e3dc65495778309fcb0821974100a10590
gaytribidwaik1406-cmyk <gaytri.bidwaik1406@gmail.com>
2025-12-16 22:30:15 +0530

    selenium

---
+++ javaselenium/src/javaselenium/java1.java
@@ -0,0 +1,16 @@
+package javaselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class java1 {
+
+	public static void main(String[] args) {
+		
+  WebDriver driver = new ChromeDriver();
+		
+		//WebDriver driver = new FirefoxDriver();
+		
+		
+		}
+}
